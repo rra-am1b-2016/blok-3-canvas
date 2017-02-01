d058e286791e5c9ce8a8a5ae90dfc4be792d1dcc
Arjan de Ruijter <rra@mboutrecht.nl>
2017-02-01 10:32:32 +0100

    staafdiagram

--- canvas.js
+++ canvas.js
@@ -4,9 +4,17 @@ var canvas = document.getElementById("myCanvas");
 // Maak een context object waarmee je objecten kunt maken
 var ctx = canvas.getContext("2d");
 
-// Geef je rectangle een kleurtje
-ctx.fillStyle = "rgb(204, 255, 153)";
-
-// Maak een rectangle
-ctx.fillRect(10, 5, 10, 140);
+// Met een for-loop maak ik nu rectangles
+for ( var i = 0; i < 14; i++)
+{
+   //alert("De waarde van i=" + i);
+   var r, g, b;
+   r = Math.floor(Math.random() * 255);
+   g = Math.floor(Math.random() * 255);
+   b = Math.floor(Math.random() * 255);
+   var color = "rgb(" + r + ", " + g + ", " + b + ")";
+   ctx.fillStyle = color;   
+   ctx.fillRect(10 + i * 20, 5 + i * 10, 10, 140 - i * 10);
+   
+}
 
