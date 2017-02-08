7cb125816ebd5aacaf07defc620b60a12ae3a1f9
Arjan de Ruijter <rra@mboutrecht.nl>
2017-02-08 08:56:06 +0100

    gradient canvas

--- Gradient/canvasgradient.js
+++ Gradient/canvasgradient.js
@@ -18,17 +18,41 @@ btn.onclick = function () {
 
    ctx.fillStyle = gradient;
    ctx.fillRect(5, 5, 590, 390);
-   
+
+   ctx.font = "50px Trebuchet";
+   ctx.fillStyle = "white";
+   ctx.fillText("Dit is een lineaire gradient", 20, 50);  
 }
 
+var counter = 1;
+var gradientRadial;
+
 var btn_1 = document.getElementsByTagName("button")[1];
-btn_1.addEventListener("click", function () {
-   var gradientRadial = ctx.createRadialGradient(300, 200, 0, 300, 200, 300);
+btn_1.addEventListener("click", function () {   
+   gradientRadial = ctx.createRadialGradient(300, 200, 0, 300, 200, counter * 10);
+   counter += 1;    
+  
    gradientRadial.addColorStop(0, "red");
    gradientRadial.addColorStop(0.3, "blue");
    gradientRadial.addColorStop(0.6, "yellow")
    gradientRadial.addColorStop(1, "green");
-
    ctx.fillStyle = gradientRadial;
    ctx.fillRect(5, 5, 590, 390);
+   if ( counter < 100) {
+      counter += 1;
+
+   }
+   else {
+      counter = 1;
+   }
+
 });
+
+setInterval(clickThatButton, 30);
+
+function clickThatButton () {
+   btn_1.click();
+   
+}
+
+
