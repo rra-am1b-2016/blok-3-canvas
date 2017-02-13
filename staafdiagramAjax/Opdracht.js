a7aff2ec75a2c1d56e07a91f45e817ece5de8668
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-02-13 09:09:13 +0100

    Opdracht

---
+++ staafdiagramAjax/Opdracht.js
@@ -0,0 +1,18 @@
+/*
+
+   1. Maak een array die 10 javascript objecten bevat. Het javascript object heeft
+      de volgende velden: id, name, numberOfContacts. Koppel dit array aan een variabele
+      'data'.
+
+   2. Sorteer je array op basis van het numberOfContacts.
+
+   3. Stop de lengte van je array in een variabele 'arrayLength'.
+
+   4. Programmeer dat de hoogte van ieder staafje overeenkomt met de waarde
+      'numberOfContacts'
+
+   5. Maak in het canvas een x-as en een y-as. Schrijf onder de x-as: leerlingen -> (horizontaal)
+      y-as: aantal geschikte bedrijven -> (verticaal)
+
+
+*/
