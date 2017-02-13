1720ec860c3c00d8ff9298635cd955e4ed1d86a1
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-02-13 09:28:32 +0100

    Staafhoogte uit array

--- staafdiagramAjax/Opdracht.js
+++ staafdiagramAjax/Opdracht.js
@@ -2,17 +2,19 @@
 
    1. Maak een array die 10 javascript objecten bevat. Het javascript object heeft
       de volgende velden: id, name, numberOfContacts. Koppel dit array aan een variabele
-      'data'.
+      'data'. &
 
-   2. Sorteer je array op basis van het numberOfContacts.
+   
+   2. Stop de lengte van je array in een variabele 'arrayLength'.
 
-   3. Stop de lengte van je array in een variabele 'arrayLength'.
-
-   4. Programmeer dat de hoogte van ieder staafje overeenkomt met de waarde
+   3. Programmeer dat de hoogte van ieder staafje overeenkomt met de waarde
       'numberOfContacts'
 
-   5. Maak in het canvas een x-as en een y-as. Schrijf onder de x-as: leerlingen -> (horizontaal)
+   4. Maak in het canvas een x-as en een y-as. Schrijf onder de x-as: leerlingen -> (horizontaal)
       y-as: aantal geschikte bedrijven -> (verticaal)
+
+   5. Sorteer je array op basis van het numberOfContacts.
+
 
 
 */
--- staafdiagramAjax/canvas.js
+++ staafdiagramAjax/canvas.js
@@ -1,3 +1,39 @@
+var data = [{"id": 1,
+             "name": "Arjan de Ruijter",
+             "numberOfContacts": 100},
+            {"id": 2,
+             "name": "Bert de Vries",
+             "numberOfContacts": 10},
+            {"id": 3,
+             "name": "Simeon Breedveld",
+             "numberOfContacts": 150},
+            {"id": 4,
+             "name": "Anke Koerts",
+             "numberOfContacts": 160},
+            {"id": 5,
+             "name": "Dinand Woest",
+             "numberOfContacts": 320},
+            {"id": 6,
+             "name": "Serre van Amersfoort",
+             "numberOfContacts": 234},
+            {"id": 7,
+             "name": "Daan de Bok",
+             "numberOfContacts": 67},
+            {"id": 8,
+             "name": "Wilco Dunner",
+             "numberOfContacts": 280},
+            {"id": 9,
+             "name": "Bas de Bakker",
+             "numberOfContacts": 330},
+             {"id": 10,
+             "name": "Arie van Ravenzwaai",
+             "numberOfContacts": 120}];
+
+// De lengte van het array data kun je opvragen met de property length
+var arrayLength = data.length;
+
+alert(data[9].numberOfContacts);
+
 // Plak een handvat op het canvas
 var canvas = document.getElementById("myCanvas");
 
